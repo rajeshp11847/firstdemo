346422460648bddac602ebe903dba0e7eedcc110
rajeshp11847 <rajeshp11847@gmail.com>
2023-04-25 12:26:57 +0530

    configration

--- src/main/java/com/rajesh/firstdemo/config/AppConfig.java
+++ src/main/java/com/rajesh/firstdemo/config/AppConfig.java
@@ -1,2 +1,14 @@
-package com.rajesh.firstdemo.config;public class AppConfig {
+package com.rajesh.firstdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
